40a8976abbbe94cd48bd157d7f2f200c686eda5b
Mateus Oliveira <mateusoli.viana@gmail.com>
2025-04-09 21:54:43 -0300

    feat: create models

---
+++ ShortURL_API/ShortURL_API/Models/Request/ShortUrlRequest.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShortURL_API.Models.Request;
+
+public class ShortUrlRequest
+{
+    [Required]
+    public string OriginalUrl { get; set; } = string.Empty;
+
+    public (bool, List<string>) Validate()
+    {
+        var errors = new List<string>();
+        if (string.IsNullOrWhiteSpace(OriginalUrl))
+        {
+            errors.Add("Original URL is required.");
+        }
+        else if (!Uri.IsWellFormedUriString(OriginalUrl, UriKind.Absolute))
+        {
+            errors.Add("Original URL is not valid.");
+        }
+
+        return (errors.Count == 0, errors);
+    }
+}
---
+++ ShortURL_API/ShortURL_API/Models/Response/ShortUrlResponse.cs
@@ -0,0 +1,6 @@
+﻿namespace ShortURL_API.Models.Response;
+
+public class ShortUrlResponse
+{
+    public string ShortUrl { get; set; } = string.Empty;
+}
