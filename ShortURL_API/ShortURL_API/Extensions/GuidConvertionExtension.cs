443779e3d816a6315855bf0410bc898f40b00981
Mateus Oliveira <mateusoli.viana@gmail.com>
2025-04-10 17:03:07 -0300

    docs: added comments and instructions

--- ShortURL_API/ShortURL_API/Extensions/GuidConvertionExtension.cs
+++ ShortURL_API/ShortURL_API/Extensions/GuidConvertionExtension.cs
@@ -6,11 +6,20 @@ namespace ShortURL_API.Extensions;
 public static class GuidConvertionExtension
 {
     private const string base62Chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-    private const int Base62Length = 62;   
+    private const int Base62Length = 62;
 
+    /// <summary>
+    /// Converts a Guid to a Base62 string representation.
+    /// </summary>
+    /// <param name="guid"></param>
+    /// <param name="maxLength"></param>
+    /// <returns></returns>
     public static string EncodeGuidToBase62(this Guid guid, int maxLength = 10)
     {
         var bytes = guid.ToByteArray();
+
+        // A zero byte is concatenated to ensure the byte array is treated as an unsigned integer
+        // when converted to a BigInteger, avoiding negative values.
         var number = new BigInteger(bytes.Concat(new byte[] { 0 }).ToArray());
 
         var sb = new StringBuilder();
