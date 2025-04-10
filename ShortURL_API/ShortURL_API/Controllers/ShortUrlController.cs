66571b7f896cce89e1df91f2a0e7ea6de705d0b6
Mateus Oliveira <mateusoli.viana@gmail.com>
2025-04-09 22:24:21 -0300

    feat: generate short url

---
+++ ShortURL_API/ShortURL_API/Controllers/ShortUrlController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShortURL_API.Extensions;
+using ShortURL_API.Models.Request;
+
+namespace ShortURL_API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ShortUrlController : ControllerBase
+{
+    private readonly IConfiguration _configuration;
+    public ShortUrlController(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    [HttpPost]
+    public IActionResult CreateShortUrl([FromBody] ShortUrlRequest request)
+    {
+        var (isValid, errors) = request.Validate();
+        if (!isValid)
+        {
+            return BadRequest(new { Errors = errors });
+        }
+       
+        var shortCode = Guid.NewGuid().EncodeGuidToBase62();
+        var baseUrl = _configuration["AppSettings:BaseUrl"];
+        var shortUrlFull = $"{baseUrl}/{shortCode}";
+
+        return Ok(new { ShortUrl = shortUrlFull });
+    }
+}
---
+++ ShortURL_API/ShortURL_API/Extensions/GuidConvertionExtension.cs
@@ -0,0 +1,26 @@
+﻿using System.Numerics;
+using System.Text;
+
+namespace ShortURL_API.Extensions;
+
+public static class GuidConvertionExtension
+{
+    private const string base62Chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    private const int Base62Length = 62;   
+
+    public static string EncodeGuidToBase62(this Guid guid, int maxLength = 10)
+    {
+        var bytes = guid.ToByteArray();
+        var number = new BigInteger(bytes.Concat(new byte[] { 0 }).ToArray());
+
+        var sb = new StringBuilder();
+
+        while (number > 0)
+        {
+            number = BigInteger.DivRem(number, Base62Length, out var remainder);
+            sb.Insert(0, base62Chars[(int)remainder]);
+        }
+
+        return sb.ToString().Substring(0, Math.Min(maxLength, sb.Length));
+    }
+}
